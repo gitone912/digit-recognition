63f700c7dd2e35a477dcae74fe167a5c70024b30
gitone912 <akash.101@kvsroranchi.in>
2024-11-15 15:52:00 +0530

    df

--- backend/digits/api/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import DigitViewSet
-
-router = DefaultRouter()
-
-router.register(r'digits', DigitViewSet)
-
-urlpatterns = router.urls
--- backend/digits/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DigitsConfig(AppConfig):
-    name = 'digits'
