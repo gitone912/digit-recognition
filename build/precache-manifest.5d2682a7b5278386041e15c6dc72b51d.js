fb16ef499365d7ddfce06b45d87a5e5b76f5b0d0
gitone912 <akash.101@kvsroranchi.in>
2024-11-15 22:44:33 +0530

    f

---
+++ build/precache-manifest.5d2682a7b5278386041e15c6dc72b51d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68f1876802dd16415d1227165ed0c2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1276bceacb6903bbcd8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c88cc7746a1b0e5296d1",
+    "url": "/static/js/2.2755b772.chunk.js"
+  },
+  {
+    "revision": "b86d02270c4e9464bbf561e5b66c18ab",
+    "url": "/static/js/2.2755b772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1276bceacb6903bbcd8",
+    "url": "/static/js/main.7a3cb6c7.chunk.js"
+  },
+  {
+    "revision": "4390948a352b0a655b6d",
+    "url": "/static/js/runtime-main.6e8bb5b1.js"
+  }
+]);
